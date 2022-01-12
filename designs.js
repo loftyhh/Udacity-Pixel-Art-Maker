2f95df5a33abfabd51b694706b86cc608f1a130e
loftyhh <92251612+loftyhh@users.noreply.github.com>
2022-01-12 05:35:12 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,46 @@
+// Select color input
+var color = document.getElementById('colorPicker');
+// make the table that forms the canvas
+var table = document.getElementById('pixelCanvas');
+//make an event listener on sizePicker based on the height and width values to call the makeGrid function
+var picker = document.getElementById('sizePicker');
+picker.addEventListener('submit', function (event) {
+  event.preventDefault();
+  //input the values of height and width to make the grid
+  var height = document.getElementById('inputHeight').value;
+  var width = document.getElementById('inputWidth').value;
+  makeGrid(height, width);
+});
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid(height, width) {
+  // For Canvas design
+  var table = document.getElementById('pixelCanvas');
+  var tablebody = document.querySelector('tbody');
+  // the makeGrid function
+  if (tablebody !== null) {
+    tablebody.remove();
+    for (let x = 0; x < height; x++) {
+      var row = table.insertRow();
+      for (let y = 0; y < width; y++) {
+        var cell = row.insertCell();
+      };
+    };
+  }
+  else {
+    for (let x = 0; x < height; x++) {
+      var row = table.insertRow();
+      for (let y = 0; y < width; y++) {
+        var cell = row.insertCell();
+      };
+    };
+  };
+  var newCells = document.querySelectorAll('td');
+  for (var square of newCells) {
+    //event listener to choose the color
+    square.addEventListener('click', function draw() {
+      var color = document.getElementById('colorPicker').value;
+      this.style.backgroundColor = color;
+    });
+  };
+};
